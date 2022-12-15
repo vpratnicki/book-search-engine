74f12b3d3819248c07055e2394bc6ac2736af4fd
vpratnicki <vanessa@pratnicki.net>
2022-12-14 19:58:47 -0500

    Chore: set up the typeDefs & resolvers for queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,24 @@
+const { User, Book } = require('../models')
+
 const resolvers = {
     Query: {
-      helloWorld: () => {
-        return 'Hello world!';
-      }
+        // get all users
+        users: async () => {
+            return User.find()
+            .select('-__v -password')
+            .populate('savedBooks');
+        },
+        // get a user by username
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+            .select('-__v -password')
+            .populate('savedBooks');
+        },
+        // get all books
+        savedBooks: async (parent, { bookId }) => {
+            return Book.find()
+            .select('-__v');
+        }
     }
   };
   
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,8 +3,29 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        authors: {
+            type: String
+        }
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+
     type Query {
-        helloWorld: String
+        users: [User]
+        user(username: String!): User
+        savedBooks(bookId: String!): [Book]
     }
     `;
 
