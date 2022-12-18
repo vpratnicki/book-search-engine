a1465d9990c3422be0f08add76b55e2690074fa0
vpratnicki <vanessa@pratnicki.net>
2022-12-18 16:19:17 -0500

    Chore: created the GET_ME query

---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+    {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                title
+                description
+                image
+                link
+            }
+        }
+    }
+`;
+Footer
