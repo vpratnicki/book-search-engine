53534c78c926319f1255edd2a810c7612ee53ac7
vpratnicki <vanessa@pratnicki.net>
2022-12-19 21:27:23 -0500

    Bug Fix: corrected book input name.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,7 +25,7 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
-  mutation saveBook($bookData: BookInput!) {
+  mutation saveBook($bookData: SavedBookInput!) {
     saveBook(bookData: $bookData) {
       _id
       username
