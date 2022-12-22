b765d295685f83e71c5469d289d8ef2832bbea1c
vpratnicki <vanessa@pratnicki.net>
2022-12-21 20:12:33 -0500

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/book-search', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/book-search', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 });
 
 module.exports = mongoose.connection;
